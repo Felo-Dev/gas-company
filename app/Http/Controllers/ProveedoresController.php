eb945a38031439be27f3fc4c4ac808ff6eec759f
Felo-Dev <brandoncortes81@gmail.com>
2024-08-28 10:02:07 -0500

    commit terminacion de controlador proveedores CRUD

--- app/Http/Controllers/ProveedoresController.php
+++ app/Http/Controllers/ProveedoresController.php
@@ -8,10 +8,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 class ProveedoresController extends Controller
 {
+
     public function index()
     {
 
-    $proveedores = Proveedores::all();
+        $proveedores = Proveedores::all();
         
         $data = [
             'proveedores' => $proveedores,
@@ -21,7 +22,8 @@ class ProveedoresController extends Controller
         return response()->json($data, 200);
     }
 
-    public function crear(Request $request){
+    public function crear(Request $request)
+    {
         $validacion = Validator::make($request->all(), [
             'empresa' => 'required|max:100',
             'pais' => 'required|max:90',
@@ -73,5 +75,144 @@ class ProveedoresController extends Controller
 
     }
 
+    public function buscar($id)
+    {
+        $proveedor = Proveedores::find($id);
 
+        if(!$proveedor) {
+            $data = [
+                'status' => 404,
+                'message' => 'No se pudo encontrar el proveedor'
+            ];
+            return response()->json($data, 400);
+        }
+
+        $data = [
+            'proveedor' => $proveedor,
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+
+    }
+
+    public function borrar($id)
+    {
+
+        if(!$id) {
+            $data = [
+                'status' => 400,
+                'message' => 'Se requiere el ID del proveedor'
+            ];
+            return response()->json($data, 400);
+        }
+        
+        $proveedor = Proveedores::find($id);
+        if(!$proveedor) {
+            $data = [
+                'status' => 404,
+                'message' => 'No se pudo encontrar el proveedor'
+            ];
+            return response()->json($data, 400);
+        }
+
+        $proveedor->delete();
+        $data = [
+            'mensaje' => "Se ha borrado el proveedor correctamente",
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function actualizar(Request $request, $id)
+    {
+        $proveedor = Proveedores::find($id);
+        if(!$proveedor) {
+            $data = [
+                'status' => 404,
+                'message' => 'No se pudo encontrar el proveedor'
+            ];
+            return response()->json($data, 400);
+        }
+
+            $validacion = Validator::make($request->all(), [
+            'empresa' => 'required|max:100',
+            'pais' => 'required|max:90',
+            'cif' => 'required|max:9',
+            'fecha_alta' => 'required|date',
+        ]);
+
+        if($validacion->fails()){
+            $data = [
+                'status' => 400,
+                'errors' => $validacion->errors(),
+                'message' => 'Error en la validacion de los datos'
+            ];
+            return response()->json($data, 400);
+        }
+
+        $proveedor->empresa = $request->empresa;
+        $proveedor->pais = $request->pais;
+        $proveedor->cif = $request->cif;
+        $proveedor->fecha_alta = $request->fecha_alta;
+        $proveedor->save();
+        $data = [
+            'proveedor' => $proveedor,
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+        
+
+    }
+
+    public function actualizarParcial( Request $request, $id)
+    {
+        $proveedor = Proveedores::find($id);
+        if(!$proveedor) {
+            $data = [
+                'status' => 404,
+                'message' => 'No se pudo encontrar el proveedor'
+            ];
+            return response()->json($data, 400);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'empresa' => 'max:100',
+            'pais' => 'max:90',
+            'cif' => 'max:9',
+            'fecha_alta' => 'date',
+        ]);
+        if ($validator->fails()) {
+            $data = [
+                'status' => 400,
+                'errors' => $validator->errors(),
+                'message' => 'Error en la validacion de los datos'
+            ];
+            return response()->json($data, 400);
+        }
+
+        if($request->has('empresa')){
+            $proveedor->empresa = $request->empresa;
+        }
+
+        if($request->has('pais')){
+            $proveedor->pais = $request->pais;
+        }
+
+        if($request->has('cif')){
+            $proveedor->cif = $request->cif;
+        }
+
+        if($request->has('fecha_alta')){
+            $proveedor->fecha_alta = $request->fecha_alta;
+        }
+
+        $proveedor->save();
+        $data = [
+            'proveedor' => $proveedor,
+            'status' => 200,
+            "mensaje" => "Se ha actualizado el proveedor correctamente"
+        ];
+        return response()->json($data, 200);
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
-<?php
+    <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProveedoresController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ProveedoresController;
 
-Route::get('/user', [ProveedoresController::class, 'index']);
-Route::post('/user', [ProveedoresController::class, 'crear']);
+    Route::get('/proveedor', [ProveedoresController::class, 'index']);
+    Route::post('/proveedor', [ProveedoresController::class, 'crear']);
+    Route::get('/proveedor/{id}', [ProveedoresController::class, 'buscar']);
+    Route::put('/proveedor/{id}', [ProveedoresController::class, 'actualizar']);
+    Route::patch('/proveedor/{id}', [ProveedoresController::class, 'actualizarParcial']);
+    Route::delete('/proveedor/{id}', [ProveedoresController::class, 'borrar']);
